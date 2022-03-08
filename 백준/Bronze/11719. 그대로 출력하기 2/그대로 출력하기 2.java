6be5561605f9d7530cf58cd09a2b1ba15e274ee8
HyunKyu Lee <88573971+wken5577@users.noreply.github.com>
2022-03-08 21:45:18 +0900

    [Bronze I] Title: 그대로 출력하기 2, Time: 244 ms, Memory: 17896 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(in);
+
+        while (sc.hasNextLine()) {
+            System.out.println(sc.nextLine());
+        }
+
+    }
+}
